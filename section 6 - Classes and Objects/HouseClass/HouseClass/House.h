ac49f659a20e0291279b91f1cea40ead11004d20
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2023-01-08 13:01:32 +0100

    Minor change to House.h

--- section 6 - Classes and Objects/HouseClass/HouseClass/House.h
+++ section 6 - Classes and Objects/HouseClass/HouseClass/House.h
@@ -6,6 +6,14 @@ using namespace std;
 
 class House {
 	public:
+
+
+		House();
+
+		House(int numStories, int numWindows, string color);
+
+		~House();
+
 		void setNumStories(int numStories);
 
 		void setNumWindows(int numWindows);
